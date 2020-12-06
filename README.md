6f607091797e97850d7fdcac836530210f3040cf
Nikola Mijajlovic <nikolamijajlovic1990@gmail.com>
2020-12-06 18:32:40 +0100

    code refactor optimizing for latest symfony versions

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-# Symfony2 Profiler shortcut
+# Symfony Profiler shortcut
 
-
-![Alt text](/ScreenShot/promo_1400.png "Symfony2 Profiler shortcut")
-
-Give access to Symfoy2 Profiler if any Response from a server is made with the "X-Debug-Token". Very useful for REST & AJAX Development/testing
+Give access to Symfony Profiler if any Response from a server is made with the "X-Debug-Token". Very useful for REST & AJAX Development/testing
 
 
 # In details
-This is an extension that help developer and/or tester to see relevant information on Symfony2 based applications.
+This is an extension that help developer and/or tester to see relevant information on Symfony based applications.
 When you enable debug mode, you probably noticed a toolBar at the bottom. Very useful, clicking on it lead the the Profiler.
 The profiler make easy to access logs and many information about the current request.
 
 This extension is here to allow you to access easily to the Profiler event if the debug toolbar is disabled or not shown.
-If you already made REST application with Symfony2  framework, you know that the Debug tool bar only show up if you render a real webPage.
+If you already made REST application with Symfony framework, you know that the Debug tool bar only show up if you render a real webPage.
 If you render JSON or XML, you just have the result and to go to the Profiler is a long way.
 
 Actually, if you look at the Response headers, you'll notice a "X-Debug-Token:0b274e"; This is the key to go to the debugger.
@@ -24,7 +21,7 @@ This extension can help you to open the profiler of AJAX/XHR request made on you
 ![Alt text](/ScreenShot/XHR.png "Multiple XHR calls")
 
 # OFFICIAL LINK
-https://chrome.google.com/webstore/detail/symfony2-debuger/denlhphadllhcolhlbbbjmhkgbknmmon
+https://chrome.google.com/webstore/detail/Symfony-debuger/denlhphadllhcolhlbbbjmhkgbknmmon
 
 
 ## Licencing
@@ -32,4 +29,4 @@ This webapp/extension and all original files contained are under [Creative Commo
 
 Thanks for VisualPharm for some icons : http://www.visualpharm.com/ [Icon link](http://icons8.com/icons/#!/416/bug)
 
-Thanks for Symfony2 and SensioLabs for SF icon : http://symfony.com/logo
+Thanks for Symfony and SensioLabs for SF icon: http://symfony.com/logo
--- sf2Debugger/js/openToken.js
+++ sf2Debugger/js/openToken.js
@@ -1,49 +1,37 @@
 (function () {
-
-
-    var createProfilerLink = function (token,target,callback) {
-        var matches = token.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-        var domain = matches && matches[1];
-        var url = 'suchi.html#domain='+domain + '&token=' + token.value;
-        if(target == null){
+    const createProfilerLink = function (token, target, callback) {
+        const matches = token.url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+        const domain = matches && matches[1];
+        let url = 'suchi.html#domain=' + domain + '&token=' + token.value;
+        if (target == null) {
             window.getConfigurationKey('defaultPage', function (defaultPage) {
-                if(defaultPage !== null && defaultPage !== 'null'){
-                    url += '&panel='+defaultPage ;
-                }else{
-                    url += '&panel=' ;
+                if (defaultPage !== null && defaultPage !== 'null') {
+                    url += '&panel=' + defaultPage;
+                } else {
+                    url += '&panel=';
                 }
-                callback.apply(this,[url]);
+                callback.apply(this, [url]);
             });
-        }else{
-            url += '?panel='+target ;
-            callback.apply(this,[url]);
+
+            return;
         }
+
+        url += '?panel=' + target;
+        callback.apply(this, [url]);
     };
 
-
-
-    /**
-     * Open a new popup/tab with the profiler in it
-     * @param token
-     */
-    var openToken = function (token,target) {
-        if(typeof target == 'undefined'){
-            target = null ;
+    window.openToken = (token, target) => {
+        if (typeof target == 'undefined') {
+            target = null;
         }
-        createProfilerLink(token,target,function(url){
+        createProfilerLink(token, target, function (url) {
             window.getConfigurationKey('profilerDestination', function (profilerDestination) {
                 if (token.popup === true || profilerDestination == 'popup') {
                     chrome.windows.create({'url': url, 'type': 'popup'});
                 } else {
                     chrome.tabs.create({'url': url});
                 }
-
             });
         });
-
     };
-
-
-    window.openToken = openToken;
-
 })();
